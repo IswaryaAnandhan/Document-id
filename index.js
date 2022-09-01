db00b7ddcd248679f01a7592db5784b1bf1284eb
IswaryaAnandhan <110915601+IswaryaAnandhan@users.noreply.github.com>
2022-09-01 14:51:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function fun(){
+    var res1=document.getElementById("First").value;
+    var res2=document.getElementById("middle").value;
+    var res3=document.getElementById("last").value;
+    var res4=document.getElementById("mail").value;
+    
+    console.log(res1);
+    console.log(res2);
+    console.log(res3);
+    console.log(res4);
+
+}
